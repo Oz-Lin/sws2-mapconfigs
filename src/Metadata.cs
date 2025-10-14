1194445b7d1c4cd06579ecaf11675abd0482f93a
cristóbal <criskywww@gmail.com>
2025-10-14 17:51:59 -0300

    feat(src): add url to plugin metadata

--- src/Metadata.cs
+++ src/Metadata.cs
@@ -7,6 +7,7 @@ namespace MapConfigs;
     Version = "1.0.0",
     Name = "MapConfigs",
     Author = "criskkky",
-    Description = "Make individualized configuration files for each map, or connect them all to a common prefix configuration."
+    Description = "Make individualized configuration files for each map, or connect them all to a common prefix configuration.",
+    Website = "https://github.com/criskkky/sws2-mapconfigs"
 )]
 public partial class MapConfigs;
